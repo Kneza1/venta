c86170b5f8f3539bb8be00881ecbf890c9154126
benjamin castro <lilbenjalemos@gmail.com>
2025-06-10 21:06:48 -0400

    finalizado

--- src/main/java/com/api/venta/VentaApplication.java
+++ src/main/java/com/api/venta/VentaApplication.java
@@ -3,7 +3,7 @@ package com.api.venta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.venta")
 public class VentaApplication {
 
 	public static void main(String[] args) {
